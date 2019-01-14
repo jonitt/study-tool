1524508f265152270e9522487137ea20d962cd51
JoniT <jonit44@gmail.com>
2019-01-14 23:41:18 +0200

    adasd

--- src/back.js
+++ src/back.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) =>
   {
-    res.send(path.resolve(__dirname, "/../frontend/dist/index.html"));
+    res.sendFile(path.resolve("/../frontend/dist/index.html"));
   }
 );
 
